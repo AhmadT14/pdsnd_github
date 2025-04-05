924c0b8f9b6544a98813c5c9073d9cb01b5c4745
Ahmad Tayeh <ahragh@gmail.com>
2025-04-05 12:35:51 +0200

    Add progress message when loading city data in load_data function

--- bikeshare.py
+++ bikeshare.py
@@ -65,7 +65,8 @@ def get_filters():
 def load_data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
-
+    Adds a progress message to notify when data is being loaded.
+    
     Args:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
@@ -73,6 +74,8 @@ def load_data(city, month, day):
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
+    
+    print(f"Loading data for {city}...")
     if city == 'chicago':
         df = pd.read_csv(CITY_DATA['chicago'])
 
